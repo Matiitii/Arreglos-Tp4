849057acc7b6731459dcd2752d992b48f199d5b4
Matii <166016459+Matiitii@users.noreply.github.com>
2024-08-12 08:10:42 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+//Act. 1
+
+
+let  Jugadores = ['May','Hannah', 'Guada','Lui', 'Tere']
+
+//Act. 2
+let Puntajes = []
+
+let indice = 0
+while (indice <= 4 ) {
+    Puntajes.push(prompt('Ingrese un puntaje'))
+    indice = indice + 1
+}
+
+
+//Act.4
+/*
+let Menor = Puntajes[0]
+for (let index = 0; index < 6; index++) {
+    if(Puntajes[index]< Menor){
+        Menor = Puntajes[index]
+    }
+    console.log(Menor)
+}
+
+//Act.3
+
+let Mayor = Puntajes[0]
+for (let index = 0; index < 6; index++) {
+    if(Puntajes[index]> Mayor){
+        Mayor = Puntajes[index]
+    }
+    console.log(Mayor)
+}
+
+//Act 5
+
+Jugadores.push(prompt('Ingrese un nombre'))
+Puntajes.push(prompt('Ingrese un puntaje'))
+
+//Act. 6 
+
+console.log(Puntajes)
+
+//Act 7
+
+Puntajes [4] = prompt('Ingrese un puntaje nuevo')
+Puntajes [5] = prompt('Ingrese un puntaje nuevo')
+
+
+*/
